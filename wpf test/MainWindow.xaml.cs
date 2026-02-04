747c825fdfc9096f1fbe129e38fa234510a9c7af
Mango67 <4422807@TE-GDA-D-D21E03>
2026-02-04 12:19:16 +0100

    + Logika apkilacji

--- wpf test/MainWindow.xaml.cs
+++ wpf test/MainWindow.xaml.cs
@@ -11,14 +11,56 @@ using System.Windows.Shapes;
 
 namespace wpf_test
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        byte red = 255;
+        byte green = 255;
+        byte blue = 255;
         public MainWindow()
         {
             InitializeComponent();
+
+            RedSlider.ValueChanged += Slider_ValueChanged;
+            GreenSlider.ValueChanged += Slider_ValueChanged_1;
+            BlueSlider.ValueChanged += Slider_ValueChanged_2;
+        }
+
+        private void SetColor()
+        {
+            Color color = Color.FromRgb(red, green, blue);
+            ColorLabel.Background = new SolidColorBrush(color);
+        }
+
+        private void SetRGBLabel()
+        {
+            RGBLabel.Background = new SolidColorBrush(Color.FromRgb(red, green, blue));
+            RGBLabel.Content = $"{red}, {green}, {blue}";   
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            red = (byte)RedSlider.Value;
+            RedLabel.Content = red.ToString();
+            SetColor();
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            green = (byte)GreenSlider.Value;
+            GreenLabel.Content = green.ToString();
+            SetColor();
+        }
+
+        private void Slider_ValueChanged_2(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            blue = (byte)BlueSlider.Value;
+            BlueLabel.Content = blue.ToString();
+            SetColor();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetRGBLabel();
         }
     }
 }
